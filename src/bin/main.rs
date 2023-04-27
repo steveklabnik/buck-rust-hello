2abd1ada7dbbc7f89cd8678eace1e07b3df2ae2f
Steve Klabnik <steve@steveklabnik.com>
2023-04-27 10:46:20 -0500

    using crates.io with buck

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,3 +1,19 @@
+use semver::{BuildMetadata, Prerelease, Version, VersionReq};
+
 fn main() {
-    hello_world::print_hello();
+    let req = VersionReq::parse(">=1.2.3, <1.8.0").unwrap();
+
+    // Check whether this requirement matches version 1.2.3-alpha.1 (no)
+    let version = Version {
+        major: 1,
+        minor: 2,
+        patch: 3,
+        pre: Prerelease::new("alpha.1").unwrap(),
+        build: BuildMetadata::EMPTY,
+    };
+    assert!(!req.matches(&version));
+
+    // Check whether it matches 1.3.0 (yes it does)
+    let version = Version::parse("1.3.0").unwrap();
+    assert!(req.matches(&version));
 }
